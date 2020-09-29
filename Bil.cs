8e99b97da1824ff996e604cc0e3ea4e47075473d
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-09-29 13:50:49 +0200

    First commit
    Initial upload

---
+++ Bil.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExcercise
+{
+    internal class Bil
+    {
+        //MaxSpeed ska vi inte kunna ända hipp som happ utanför klassen
+        public int MaxSpeed { get; private set; }
+        public string Colour { get; set; }
+        public int Speed { get; private set; }
+
+        //Vi skapar en konstruktor som TVINGAR den som använder klassen
+        //att sätta en maxhastighet. Ingen kan nu använda en instans av
+        //klassen Bil utan att ange en maxhastighet
+        public Bil(int maxSpeed)
+        {
+            MaxSpeed = maxSpeed;
+            Colour = string.Empty;
+            Speed = 0;
+        }
+
+        public int Drive(int speed)
+        {
+            //Om den hastighet vi försöker få upp bilen i övergår
+            //högsta tillåtna hastighet, sätter vi hastigheten till 
+            //maxhastighet.
+            if (speed > MaxSpeed)
+            {
+                Speed = MaxSpeed;
+            }
+            else
+            {
+                Speed = speed;
+            }
+
+            return Speed;
+        }
+
+        //Här skulle vi kunna skapa en Trim-metod eller liknande om vi vill kunna ändra
+        //MaxSpeed på något sätt
+        public int TrimCar(int newMaxSpeed)
+        {
+            if (newMaxSpeed > 0)
+            {
+                //Vi kommer åt propertyns privata set eftersom vi är inne i klassen
+                MaxSpeed = newMaxSpeed;
+            }
+
+            return MaxSpeed;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nu måste vi använda konstruktorn för att sätta ett värde på maxhastighet
+            //Alla andra instansvariabler får korrekta "default"-värden 
+            Bil bil = new Bil(220);
+            bil.Colour = "Svart";
+            bil.Drive(180);
+
+            Console.WriteLine($"Vi kör nu i {bil.Speed}km/h");
+            //Går inte för maxspeed är just nu 220
+            bil.Drive(250);
+            Console.WriteLine($"Vi försöker gasa över maxhastighet, men kör nu i {bil.Speed}km/h");
+
+            bil.TrimCar(260);
+            //Nu kan vi komma upp i 250, för maxspeed sattes till 260 via Trim-metoden!
+            bil.Drive(250);
+            Console.WriteLine($"Nu är bilen trimmad så vi kan köra i {bil.Speed}km/h");
+
+            Console.ReadLine();
+        }
+    }
+}
